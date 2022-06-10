bdab3cc68f39675d6c6d1cc0fc04435d24b04ecc
Marwane Beriich <marber.mb11@gmail.com>
2022-06-10 18:55:31 +0100

    add CORS

--- src/main/java/com/example/customerservice/repository/CustomerRepository.java
+++ src/main/java/com/example/customerservice/repository/CustomerRepository.java
@@ -3,7 +3,9 @@ package com.example.customerservice.repository;
 import com.example.customerservice.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins="http://localhost:3000")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
